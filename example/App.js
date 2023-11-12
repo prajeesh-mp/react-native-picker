f6108cbb4330a1ad7e0dc9c2f9c713bc5d7d90f2
Prajeesh <meetprajeesh@gmail.com>
2023-11-12 14:12:33 +0530

    Update: FlatList, Example

--- example/App.js
+++ example/App.js
@@ -1,31 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
 import Picker from "react-native-picker";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <StatusBar style="auto" />
-      <Picker
-        items={[
-          { label: "One", value: 1 },
-          { label: "Two", value: 2 },
-          { label: "Three", value: 3 },
-          { label: "Four", value: 4 },
-        ]}
-        onSelect={(item) => console.log(item)}
-      />
-    </View>
-  );
+    const [selected, setSelected] = useState(null);
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Text style={{ textAlign: "center" }}>React Native Picker</Text>
+            <Text style={{ textAlign: "center" }}>Simple DropDown for both iOS and Android</Text>
+            <Picker
+                items={[
+                    { label: "One", value: 1 },
+                    { label: "Two", value: 2 },
+                    { label: "Three", value: 3 },
+                    { label: "Four", value: 4 }
+                ]}
+                onSelect={(item) => setSelected(item)}
+                // selectedItem={selected}
+            />
+            <Text>Selected: </Text>
+            <Text>{JSON.stringify(selected)}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 40,
-  },
+    container: {
+        flex: 0.5,
+        justifyContent: "space-between",
+        paddingVertical: 80,
+        paddingHorizontal: 20
+    }
 });
